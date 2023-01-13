cc0a2c583eac478036e38ef6e07c5515a3e15465
Jahirul Islam <jahirul@elyranur.com>
2023-01-14 00:14:23 +0600

    footer added

--- src/Pages/Home/Home/AboutUs.js
+++ src/Pages/Home/Home/AboutUs.js
@@ -3,13 +3,13 @@ import React from 'react';
 const AboutUs = () => {
     return (
         <div className='w-11/12 mx-auto my-28 lg:flex justify-center'>
-            <div className='w-6/12'>
+            <div className=' w-5/6 lg:w-6/12'>
                 <h1 className='text-5xl mb-3 font-semibold'>About Our Story</h1>
                 <p>We are one of the leading laptop reselling companies in Bangladesh. We provide a wide range of refurbished laptops that include refurbished laptops, second hand/refurbished computers and original brand new laptops at incredible low prices.</p>
                 <br />
                 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip consequat.</p>
             </div>
-            <div className='w-6/12 ml-5 justify-center'>
+            <div className='w-11/12 lg:w-6/12 ml-5 justify-center'>
                 <img className=' h-80' src='https://www.freepnglogos.com/uploads/laptop-png/laptop-png-intertech-office-automations-distribution-inc-vancouver-11.png' alt="" />
             </div>
         </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>This is Footer</h2>
-        </div>
-    );
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content rounded-lg">
+      <div>
+        <Link className="font-bold normal-case text-2xl">
+          <Link to="/">
+            <span className="text-orange-600">Laptop</span> Bazar
+          </Link>
+        </Link>
+        <p>
+          Welcome to Laptop Bazar
+          <br />
+         
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">Services</span>
+        <Link className="link link-hover">Branding</Link>
+        <Link className="link link-hover">Design</Link>
+        <Link className="link link-hover">Marketing</Link>
+        <Link className="link link-hover">Advertisement</Link>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <Link className="link link-hover">About us</Link>
+        <Link className="link link-hover">Contact</Link>
+        <Link className="link link-hover">Jobs</Link>
+        <Link className="link link-hover">Press kit</Link>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <Link className="link link-hover">Terms of use</Link>
+        <Link className="link link-hover">Privacy policy</Link>
+        <Link className="link link-hover">Cookie policy</Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
